91b12318e6b57baa41c0f942c8f022913449127e
adilth <rajaadil1995@gmail.com>
2025-06-20 19:36:31 +0100

    first commit

--- src/component/hero/HeroSection.tsx
+++ src/component/hero/HeroSection.tsx
@@ -3,21 +3,23 @@ import heroImg from "../../assets/png/hero-section-image.png";
 
 const HeroSection = () => (
   <section className={`${styles.heroSection} container`}>
-    <div className={styles.heroContent}>
-      <h1 className={styles.heading}>People First</h1>
-      <p className={styles.description}>
-        Convaincus que la création de valeur est une co-construction avec le capital
-        humain d'une organisation publique ou privée, DEO Conseil a pour raison d'être de
-        faire grandir les hommes et les organisations en donnant du sens à toutes les
-        parties prenantes.
-      </p>
-      <a href="#catalogue" className={styles.ctaBtn}>
-        Notre Calatogue
-      </a>
-    </div>
-    <div className={styles.heroImageWrapper}>
-      <img src={heroImg} alt="People working together" className={styles.heroImage} />
-      {/* <img src={bgImg} alt="Background" className={styles.bgImage} /> */}
+    <div className="">
+      <div className={styles.heroContent}>
+        <h1 className={styles.heading}>People First</h1>
+        <p className={styles.description}>
+          Convaincus que la création de valeur est une co-construction avec le capital
+          humain d'une organisation publique ou privée, DEO Conseil a pour raison d'être
+          de faire grandir les hommes et les organisations en donnant du sens à toutes les
+          parties prenantes.
+        </p>
+        <a href="#catalogue" className={styles.ctaBtn}>
+          Notre Calatogue
+        </a>
+      </div>
+      <div className={styles.heroImageWrapper}>
+        <img src={heroImg} alt="People working together" className={styles.heroImage} />
+        {/* <img src={bgImg} alt="Background" className={styles.bgImage} /> */}
+      </div>
     </div>
   </section>
 );
